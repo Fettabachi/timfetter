e90665257268edd8022fccd019cb20e1110ee8c3
Tim Fetter <timfettermail@gmail.com>
2026-04-10 07:50:10 -0400

    Brought over the ACF Blocks from fictional university which includes the json and the ACF exports Brought over the styles and JS and had to do a lot of edits to get them to align with the structure rendered for my site Deactivated the classic editor and reactivated Gutenberg Added a page.php

--- page.php
+++ page.php
@@ -2,27 +2,14 @@
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-
         <?php if (have_posts()) : ?>
-
-            <?php /* Start the Loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
-
-                <?php get_template_part('content', get_post_format()); ?>
-
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <?php the_content(); ?>
+                </article>
             <?php endwhile; ?>
-
-            <?php //base_paging_nav(); 
-            ?>
-
-        <?php else : ?>
-
-            <?php get_template_part('content', 'none'); ?>
-
         <?php endif; ?>
-
     </main><!-- #main -->
 </div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
