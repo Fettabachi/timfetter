c3eae88c196b6fc62d59933fb13bcfb238fcdf96
Tim Fetter <timfettermail@gmail.com>
2026-05-30 15:22:29 -0400

    Added new favicon to site

--- header.php
+++ header.php
@@ -4,12 +4,15 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/favicons/manifest.json">
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="icon" type="image/x-icon" href="<?php echo esc_url(get_theme_file_uri('/assets/favicons/favicon.ico')); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(get_theme_file_uri('/assets/favicons/favicon-32x32.png')); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(get_theme_file_uri('/assets/favicons/favicon-16x16.png')); ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(get_theme_file_uri('/assets/favicons/apple-touch-icon.png')); ?>">
+    <link rel="manifest" href="<?php echo esc_url(get_theme_file_uri('/assets/favicons/site.webmanifest')); ?>">
+
+    <meta name="theme-color" content="#0d3b66">
+    <meta name="msapplication-TileColor" content="#0d3b66">
+    <meta name="msapplication-config" content="<?php echo esc_url(get_theme_file_uri('/assets/favicons/browserconfig.xml')); ?>">
     <title>Tim Fetter | Web Developer</title>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HKFVCPJGEY"></script>
